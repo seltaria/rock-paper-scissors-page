8c650dbeb06e666c5d59fea1394ddc8de982fa2b
Бондарева Анастасия <seltaria@mail.ru>
2023-01-18 13:04:00 +0300

    Updates

---
+++ sw.js
@@ -0,0 +1,39 @@
+const staticCacheName = 'rps-app';
+const assetUrls = [
+  '/index.html',
+  '/static/css/main.b20be89e.css',
+  '/static/js/main.ef06e0a8.js',
+  // '../src/index.css',
+  // '../src/index.js',
+  // '../src/App.js',
+  // '../src/store/reducers.js',
+  // '../src/store/store-reducer.js',
+  // '../src/components/Button.js',
+  // '../src/components/Header.js',
+  // '../src/components/Main.js',
+  // '../src/components/Picked.js',
+  // '../src/components/Rules.js',
+]
+
+self.addEventListener('install', async event => {
+  const cache = await caches.open(staticCacheName);
+  await cache.addAll(assetUrls);
+})
+
+self.addEventListener('activate', async event => {
+  const cacheNames = await caches.keys();
+  await Promise.all(
+    cacheNames
+      .filter(name => name !== staticCacheName)
+      .map(name => caches.delete(name))
+  )
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(cacheFirst(event.request))
+})
+
+async function cacheFirst(request) {
+  const cached = await caches.match(request);
+  return cached ?? await fetch(request);
+}
