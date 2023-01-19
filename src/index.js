5cca16bccb7b687f4ec9fb125ed0466853b3751d
Бондарева Анастасия <seltaria@mail.ru>
2023-01-19 15:36:00 +0300

    add service worker

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ import { createStore } from 'redux';
 import './index.css';
 import { App } from './App';
 import { rootReducer } from './store/reducers';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const store = createStore(rootReducer);
+
+serviceWorkerRegistration.register();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
